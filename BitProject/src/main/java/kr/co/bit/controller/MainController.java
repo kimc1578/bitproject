4572ef031c8a3a6279e9a1dea3e88796260d9841
kimc1578 <kimc1578@naver.com>
2017-06-29 14:31:44 +0900

    test

--- BitProject/src/main/java/kr/co/bit/controller/MainController.java
+++ BitProject/src/main/java/kr/co/bit/controller/MainController.java
@@ -21,7 +21,7 @@ public class MainController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
+		  
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
